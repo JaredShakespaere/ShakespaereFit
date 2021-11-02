70208ab76cb731fa00d54f59af14ae3153e906dd
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-11-02 15:49:45 -0500

    deployment prep

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -11,6 +12,19 @@ const ctrl = require('./ctrl')
 app.post("/api/user", ctrl.addUser)
 app.get("/api/shop", ctrl.getProduct)
 
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "../homePage.html"))
 
-const port = 4400
-app.listen(port, console.log(`Running on port ${port}`))
+})
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "../shop.html"))
+
+})
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.css'))
+  })
+
+
+  const port = process.env.PORT || 4400
+
+app.listen(port, () => console.log(`Running on port ${port}`))
